f4c476bf2f6ba666ac7846e1d86f3cbebf252c00
xzotacx <jesus.152006@gmail.com>
2021-02-21 15:41:08 -0400

    Arreglado menu hamburguesa

--- menu-hamburguesa/app.js
+++ menu-hamburguesa/app.js
@@ -1,14 +1,19 @@
-const btnHamburger = document.querySelector("#btnHamburger");
-const header = document.querySelector(".header");
+const $btnHamburger = document.querySelector("#btnHamburger");
+const $header = document.querySelector(".header");
+const $menu = document.querySelector(".menu");
 
-btnHamburger.addEventListener('click', function(){
-  console.log('click hamburger');
+console.log($header);
+console.log($btnHamburger);
+$btnHamburger.addEventListener("click", function () {
+  console.log("click hamburger");
 
-  if(header.classList.contains("open")){ // Close Hamburger Menu
-    header.classList.remove("open");    
- }
-
-  else { // Open Hamburger Menu
-    header.classList.add("open");
-}  
+  if ($header.classList.contains("open")) {
+    // Close Hamburger Menu
+    $header.classList.remove("open");
+    $menu.classList.remove("show");
+  } else {
+    // Open Hamburger Menu
+    $header.classList.add("open");
+    $menu.classList.add("show");
+  }
 });
